c45bbe2557f32a00bbc90494aab26279b2101cc9
Andrew <andrey_nugaev@list.ru>
2018-11-30 19:15:50 +0500

    font-size,html

--- subs/static/subs/register_script.js
+++ subs/static/subs/register_script.js
@@ -1,6 +1,6 @@
-var good_login = false, good_mail = false, good_school = false, good_password = false, good_repeat_password = false,
- repeat_password_val='', password_val='', username='', school='',
- good_log_mail = false, good_log_password = false,  log_password = '';
+var good_login = false, good_mail = false, good_Lname = false,good_Fname = false, good_password = false, good_repeat_password = false,
+ repeat_password_val='', password_val='', username='', last_name='', first_name = '',
+ good_log_login = false, good_log_password = false,  log_password = '', log_login='';
 
 function check_name(){
     username = document.getElementById('user_login').value;
@@ -109,22 +109,36 @@ function check_len_password() {
     check_repeat_password()
 }
 
-function check_school() {
-    school = document.getElementById('input_school').value;
-    if (school.length==0){
-        document.getElementById('true_school').innerText='Заполните поле';
-        input_school.style.borderColor='red';
-        good_school = false;
+function check_Fname() {
+    first_name = document.getElementById('input_Fname').value;
+    if (first_name.length==0){
+        document.getElementById('true_Fname').innerText='Заполните поле';
+        input_Fname.style.borderColor='red';
+        good_Fname = false;
     } else{
-        document.getElementById('true_school').innerText='';
-        input_school.style.borderColor='lightgreen';
-        good_school = true;
+        document.getElementById('true_Fname').innerText='';
+        input_Fname.style.borderColor='lightgreen';
+        good_Fname = true;
+    }
+
+}
+
+function check_Lname() {
+    last_name = document.getElementById('input_Lname').value;
+    if (last_name.length==0){
+        document.getElementById('true_Lname').innerText='Заполните поле';
+        input_Lname.style.borderColor='red';
+        good_Lname = false;
+    } else{
+        document.getElementById('true_Lname').innerText='';
+        input_Lname.style.borderColor='lightgreen';
+        good_Lname = true;
     }
 
 }
 
 function check_submit() {
-    if (good_school==true && good_password==true && good_repeat_password==true && good_mail==true && good_login==true) {
+    if (good_Fname==true && good_Lname==true && good_password==true && good_repeat_password==true && good_mail==true && good_login==true) {
         return true;
     } else{
         alert('Вы что-то пропустили');
@@ -133,8 +147,9 @@ function check_submit() {
 }
 
 function submit_login() {
-    $(document).find('#e_mail_log').change();
-    if (good_log_mail==true && good_log_password==true){
+    check_log_login();
+    check_log_pas();
+    if (good_log_login==true && good_log_password==true){
         return true;
     } else{
         alert('Что-то пошло не так. Проверьте вводимые поля');
@@ -142,24 +157,6 @@ function submit_login() {
     }
 
 }
-
-$(document).ready(function () {
-    $('#e_mail_log').change(function () {
-        if ($(this).val() != '') {
-            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-            if (pattern.test($(this).val())) {
-                $(this).css({'border-color':'lightgreen'});
-                good_log_mail = true;
-            } else {
-                $(this).css({'border': '2px solid #ff0000'});
-                good_log_mail = false;
-            }
-        } else {
-            $(this).css({'border': '2px solid #ff0000'});
-            good_log_mail = false;
-        }
-    });
-});
 
 function check_log_pas() {
     log_password = document.getElementById('password_log').value;
@@ -169,5 +166,16 @@ function check_log_pas() {
     } else{
         $('#password_log').css('border-color' , 'lightgreen');
         good_log_password = true;
+    }
+}
+
+function check_log_login() {
+    log_login = document.getElementById('login_log').value;
+    if (log_login.length == 0) {
+        good_log_login = false;
+        $('#login_log').css('border-color' , 'red');
+    } else{
+        $('#login_log').css('border-color' , 'lightgreen');
+        good_log_login= true;
     }
 }
--- subs/views.py
+++ subs/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import children
 from django.contrib.auth.models import User
 from django.contrib import auth
 
@@ -43,16 +42,16 @@ def zero(request):
 
 def signup(request):
     login = request.POST.get("login", False)
-    school = request.POST.get("school", False)
+    Lname = request.POST.get("last_name", False)
+    Fname = request.POST.get("first_name", False)
     mail = request.POST.get("mail", False)
     password = request.POST.get("password", False)
     repeat_password = request.POST.get("repeat_password", False)
 
     if password == repeat_password:
-        registr = User.objects.create_children(childrenname='Vasya', email='Vasya@list.ru', password='qwerty1234')
+        registr = User.objects.create_user(email=mail, username=login, password=password, first_name=Fname,
+                                           last_name=Lname)
         registr.save()
-        reg = children(school_name=school, children_name=login, e_mail=mail, password=password)
-        reg.save()
         return HttpResponseRedirect('/main')
     elif password != repeat_password:
         return HttpResponseRedirect('/')
@@ -60,7 +59,7 @@ def signup(request):
 
 def check_login(request):
     login = request.GET.get('login', False)
-    user = children.objects.values_list('children_name', flat=True)  # выборка значений столбца из базы данных
+    user = User.objects.values_list('username', flat=True)  # выборка значений столбца из базы данных
     if login not in user:
         return HttpResponse('good', content_type='text/html')
     else:
@@ -69,29 +68,21 @@ def check_login(request):
 
 def check_mail(request):
     mail = request.GET.get('mail', False)
-    children_mail = children.objects.values_list('e_mail', flat=True)
-    if mail not in children_mail:
+    user_mail = User.objects.values_list('email', flat=True)
+    if mail not in user_mail:
         return HttpResponse('good', content_type='text/html')
     else:
         return HttpResponse('bad', content_type='text/html')
 
 
 def login_user(request):
-    mail = request.POST.get('e_mail_log', False)
+    login = request.POST.get('login_log', False)
     password = request.POST.get('password_log', False)
-    mass = (children.objects.filter(e_mail=mail).value())
-    pas = (list((mass[0]).values()))[4]
-    login = (list((mass[0]).values()))[2]
-    if pas == password:
-        print(login,pas)
-        user = auth.authenticate(username=login, password=pas)
+    user = auth.authenticate(username=login, password=password)
+    if user is not None:
+        auth.login(request, user)
         print(user)
-        if user is not None:
-            print('Авторизация прошла')
-            auth.login(request,user)
-            return HttpResponseRedirect('/main/')
-        else:
-            print('сработало иначе')
-            return HttpResponseRedirect('/main/login/')
+        return HttpResponseRedirect('/main/')
     else:
+        print('сработало иначе')
         return HttpResponseRedirect('/main/login/')
